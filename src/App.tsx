7c96d36e0603bde5efa37205fdc7e2920bb969ab
Ignacio Bardin <ilbardin@gmail.com>
2023-03-06 20:19:01 -0300

    - Changed App.js to .tsx extension. - Implemented SearchBar. - Added constants file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Box from '@mui/material';
+import {Box} from '@mui/material';
 import Navbar from "./components/Navbar";
 import Feed from "./components/Feed";
 import VideoDetail from "./components/VideoDetail";
 import ChannelDetail from "./components/ChannelDetail";
 import SearchFeed from "./components/SearchFeed";
+import React from "react";
 
 
 const App = () => (
